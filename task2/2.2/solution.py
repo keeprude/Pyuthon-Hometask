837102e514e9894f0ea62abea1d4a55c7905d3b2
keeprude <32794994+keeprude@users.noreply.github.com>
2019-10-25 09:16:18 +0300

    Update solution.py

--- task2/2.2/solution.py
+++ task2/2.2/solution.py
@@ -1,30 +1,33 @@
-#create a dictionary.
-d = {}
-#reading and inserting keys(words) with appropriate value(number of use).
-while 1:
-    line = input().split()
-    if (not line) :
-        break
-    for string in line:
-        if (string) :
-            if (string in d) :
-                d[string] += int(1)
-            else :
-                d[string] = int(1)
-        else :
-            break
-flag = 0
-max = 0
-#searching for the most common word.
-for word in d:
-    if (d[word] == max) :
-        flag = 1
-    if (d[word] > max) :
-        max = d[word]
-        result = word
-        flag = 0
+"""solution"""
 
-if (flag == 1) :
-    print('-')
-else :
-    print(result)
+if __name__ == "__main__":
+    DICT = {}
+    #reading and inserting keys(words) with appropriate value(number of use).
+    while 1:
+        LINE = input().split()
+        if not LINE:
+            break
+        for string in LINE:
+            if string:
+                if string in DICT:
+                    DICT[string] += int(1)
+                else:
+                    DICT[string] = int(1)
+            else:
+                break
+    FLAG = 0
+    MAX_NUMBER_OF_USE = 0
+    RESULT = ""
+    #searching for the most common word.
+    for word in DICT:
+        if DICT[word] == MAX_NUMBER_OF_USE:
+            FLAG = 1
+        if DICT[word] > MAX_NUMBER_OF_USE:
+            MAX_NUMBER_OF_USE = DICT[word]
+            RESULT = word
+            FLAG = 0
+
+    if FLAG == 1:
+        print('-')
+    else:
+        print(RESULT)
