6d4b94524e3dde8cac694e1ae7a5ae13502c0266
keeprude <32794994+keeprude@users.noreply.github.com>
2019-10-25 09:19:50 +0300

    Update solution.py

--- task3/sequential/solution.py
+++ task3/sequential/solution.py
@@ -1,12 +1,16 @@
+"""solution"""
+
 import os
 import time
 import librosa
 import numpy
 
-data_path = "/Users/keeprude/Desktop/tasktest"
-answer_path = "/Users/keeprude/Desktop/taskanswer"
+DATA_PATH = "/Users/keeprude/Desktop/tasktest"
+ANSWER_PATH = "/Users/keeprude/Desktop/taskanswer"
 
 def mfcc(data_path, answer_path):
+    """extracting mfcc"""
+
     list_of_obj = os.listdir(data_path)
     for obj in list_of_obj:
         path = data_path + '/' + obj
@@ -19,11 +23,11 @@ def mfcc(data_path, answer_path):
             temp = answer_path + '/' + obj[:len(obj) - 4]
             print(temp)
             y, sr = librosa.load(path)
-            arr = librosa.feature.mfcc(y = y, sr = sr)
+            arr = librosa.feature.mfcc(y=y, sr=sr)
             numpy.save(temp, arr)
 
-
-start_time = time.time()
-mfcc(data_path, answer_path)
-finish_time = time.time()
-print(finish_time - start_time)
+if __name__ == "__main__":
+    START_TIME = time.time()
+    mfcc(DATA_PATH, ANSWER_PATH)
+    FINISH_TIME = time.time()
+    print(FINISH_TIME - START_TIME)
