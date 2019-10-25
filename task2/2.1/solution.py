11917247d93dc03888244d996134d9db606acc15
keeprude <32794994+keeprude@users.noreply.github.com>
2019-10-25 09:15:13 +0300

    Update solution.py

--- task2/2.1/solution.py
+++ task2/2.1/solution.py
@@ -1,17 +1,22 @@
-s = input()
-length = len(s)
-#selecting substring with the length: len(string) = x * len(substring), where x is Natural number and 
-#checking the equality.
-for x in range(length) :
-    sample = s[:(length - x)]
-    sample_length = len(sample)
-    if (length % sample_length != 0) :
-        continue
-    num = length / sample_length
-    if (sample * int(num) == s) :
-        result = num
-    else :
-        continue
+"""solution"""
 
-print(int(result))
-    
+if __name__ == "__main__":
+    INPUT_STRING = input()
+    LENGTH = len(INPUT_STRING)
+    RESULT = ""
+
+    """selecting substring with the length: len(string) = x * len(substring),
+    where x is Natural number and checking the equality"""
+
+    for x in range(LENGTH):
+        sample = INPUT_STRING[:(LENGTH - x)]
+        sample_length = len(sample)
+        if LENGTH % sample_length != 0:
+            continue
+        num = LENGTH / sample_length
+        if sample * int(num) == INPUT_STRING:
+            RESULT = num
+        else:
+            continue
+
+    print(int(RESULT))
