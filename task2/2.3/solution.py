671837c5b214e8e5531cbc10f5b63cf10b577f6e
keeprude <32794994+keeprude@users.noreply.github.com>
2019-10-25 09:17:21 +0300

    Update solution.py

--- task2/2.3/solution.py
+++ task2/2.3/solution.py
@@ -1,19 +1,31 @@
+"""solution"""
+
 from itertools import product
 
-def combinations (num) :
-    dictionary = {0 : ('_'), 1 : (''), 2 : ('a', 'b', 'c'), 3 : ('d', 'e', 'f'), 4 : ('g', 'h', 'i'),
-        5 : ('j', 'k', 'l'), 6 : ('m', 'n', 'o'), 7 : ('p', 'q', 'r', 's'), 
-        8 : ('t', 'u', 'v'), 9 : ('w', 'x', 'y', 'z')}
+def combinations(num):
+    """prints all the possible combinations of letters"""
+
+    dictionary = {0 : ('_'),
+                  1 : (''),
+                  2 : ('a', 'b', 'c'),
+                  3 : ('d', 'e', 'f'),
+                  4 : ('g', 'h', 'i'),
+                  5 : ('j', 'k', 'l'),
+                  6 : ('m', 'n', 'o'),
+                  7 : ('p', 'q', 'r', 's'),
+                  8 : ('t', 'u', 'v'),
+                  9 : ('w', 'x', 'y', 'z')}
     arrays = []
-    for x in num :
-        if (x == '1') :
+    for digit in num:
+        if digit == '1':
             continue
-        arrays.append(dictionary[int(x)])
+        arrays.append(dictionary[int(digit)])
     temp = list(product(*arrays))
     result = []
-    for pair in temp :
+    for pair in temp:
         result.append(''.join(pair))
     print(result)
 
-x = input()
-combinations(x)
+if __name__ == "__main__":
+    INPUT_NUMBER = input()
+    combinations(INPUT_NUMBER)
