dfb3b045520b23f122b11e0906dad5c2e217ae22
keeprude <32794994+keeprude@users.noreply.github.com>
2019-10-25 09:18:03 +0300

    Update solution.py

--- task2/2.4/solution.py
+++ task2/2.4/solution.py
@@ -1,34 +1,43 @@
+"""solution"""
+
 from collections import defaultdict
 
 class Graph:
-    def __init__(self, edges) :
+    """implementation of graph"""
+
+    def __init__(self, edges):
         self.node = defaultdict(list)
-        for edge in edges :
+        for edge in edges:
             self.node[edge[0]].append(edge[1])
             self.node[edge[1]].append(edge[0])
 
 #self.node = {keys ~ nodes}
 #self.node[node] = {values connected to this key ~ destinations}
-    def bfs(self) :
-        visited = ''
-        for node in self.node :
-            if not str(node) in visited :
+
+    def bfs(self):
+        """breadth-first search"""
+        
+       visited = ''
+        for node in self.node:
+            if str(node) not in visited:
                 print(node)
                 visited += str(node)
-            for x in self.node[node] :
-                if not str(x) in visited :
-                    print(x)
-                    visited += str(x)
+            for vertex in self.node[node]:
+                if str(vertex) not in visited:
+                    print(vertex)
+                   visited += str(vertex)
 
-    def dfs(self, v) :
+    #def dfs(self, v):
         #troubles with implementation.
 
-    def check(self) :
+    def check(self):
+        """prints the Graph"""
+
         print(self.node)
 
-edges = [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4], [1, 6], [6, 7], [6, 8]]
-graph = Graph(edges)
-graph.bfs() # result : 0 3 1 2 4 6 5 7 8
-print('\n')
-graph.dfs(0) # result : 
-
+if __name__ == "__main__":
+    INPUT_EDGES = [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4], [1, 6], [6, 7], [6, 8]]
+    GRAPH = Graph(INPUT_EDGES)
+    GRAPH.bfs() # result : 0 3 1 2 4 6 5 7 8
+    print('\n')
+    #graph.dfs(0) # result :
