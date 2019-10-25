fa5f6993e1146c1d6076cfe889cc1d4447844b16
keeprude <32794994+keeprude@users.noreply.github.com>
2019-10-25 09:21:31 +0300

    Update solution.py

--- task3/process/solution.py
+++ task3/process/solution.py
@@ -1,13 +1,17 @@
+""""solution"""
+
+from multiprocessing import Process
 import os
 import time
 import librosa
 import numpy
-from multiprocessing import Process
 
-data_path = "/Users/keeprude/Desktop/tasktest/aac"
-answer_path = "/Users/keeprude/Desktop/answertest3"
+DATA_PATH = "/Users/keeprude/Desktop/tasktest/aac"
+ANSWER_PATH = "/Users/keeprude/Desktop/answertest3"
 
 def mfcc(data_path, answer_path):
+    """extracting mfcc"""
+
     list_of_obj = os.listdir(data_path)
     for obj in list_of_obj:
         path = data_path + '/' + obj
@@ -20,38 +24,40 @@ def mfcc(data_path, answer_path):
             temp = answer_path + '/' + obj[:len(obj) - 4]
             print(temp)
             y, sr = librosa.load(path)
-            arr = librosa.feature.mfcc(y = y, sr = sr)
+            arr = librosa.feature.mfcc(y=y, sr=sr)
             numpy.save(temp, arr)
 
 def mfcc_iter(data_path, answer_path, start, stop):
+    """mfcc for a portion of dirs"""
+
     list_of_obj = os.listdir(data_path)
     for i in range(start, stop):
         tmp_data = data_path + '/' + list_of_obj[i]
         tmp_save = answer_path + '/' + list_of_obj[i]
-        if not os.path.exists(tmp_save) :
+        if not os.path.exists(tmp_save):
             os.mkdir(tmp_save)
         mfcc(tmp_data, tmp_save)
 
 if __name__ == "__main__":
 
-    list_of_files = os.listdir(data_path)
-    length = len(list_of_files)
+    LIST_OF_FILES = os.listdir(DATA_PATH)
+    LENGTH = len(LIST_OF_FILES) 
 
-    process1 = Process(target = mfcc_iter, args = (data_path, answer_path, 0, length // 4, ))
-    process2 = Process(target = mfcc_iter, args = (data_path, answer_path, length // 4, length // 2, ))
-    process3 = Process(target = mfcc_iter, args = (data_path, answer_path, length // 2, 3 * length // 4, ))
-    process4 = Process(target = mfcc_iter, args = (data_path, answer_path, 3 * length // 4, length, ))
+    PROCESS1 = Process(target=mfcc_iter, args=(DATA_PATH, ANSWER_PATH, 0, LENGTH//4,))
+    PROCESS2 = Process(target=mfcc_iter, args=(DATA_PATH, ANSWER_PATH, LENGTH//4, LENGTH//2,))
+    PROCESS3 = Process(target=mfcc_iter, args=(DATA_PATH, ANSWER_PATH, LENGTH//2, 3*LENGTH//4,))
+    PROCESS4 = Process(target=mfcc_iter, args=(DATA_PATH, ANSWER_PATH, 3*LENGTH//4, LENGTH,))
 
-    start_time = time.time()
-    process1.start()
-    process2.start()
-    process3.start()
-    process4.start()
+    START_TIME = time.time()
+    PROCESS1.start()
+    PROCESS2.start()
+    PROCESS3.start()
+    PROCESS4.start()
 
-    process1.join()
-    process2.join()
-    process3.join()
-    process4.join()
-    finish_time = time.time()
+    PROCESS1.join()
+    PROCESS2.join()
+    PROCESS3.join()
+    PROCESS4.join()
+    FINISH_TIME = time.time()
 
-    print(finish_time - start_time)
+    print(FINISH_TIME - START_TIME)
